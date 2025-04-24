9ce7e3117ec6bbdedb58ed7b131c7a3c66d129af
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-24 17:33:33 +0100

    Implement listing transactions for an account
    Also actually run main() oops

--- support-bank.py
+++ support-bank.py
@@ -9,6 +9,7 @@ def update_accounts(file, accounts):
     with open(file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
+            payer, payee = False, False
             for account in accounts:
                 payer = False
                 payee = False
@@ -36,19 +37,29 @@ def main():
     while command != "Exit":
         command = input('Enter command: ')
 
+        # List all account balances
         if command == 'List All':
             for account in accounts:
-                print(f'Name: {account.name}, Balance: {account.balance}')
+                print(f'Name: {account.name}, Balance: £{account.balance:0.2f}')
+
+        # List transactions that involve a specific account
         elif command.startswith('List ') and len(command) >= 6:
             for account in accounts:
                 if command[5:] == account.name:
                     with open(transactions, newline='') as csvfile:
                         reader = csv.DictReader(csvfile)
                         for row in reader:
-                            
-
+                            if row['From'] == account.name:
+                                print(f'Date: {row["Date"]}, To: {row["To"]}, Amount: £-{float(row["Amount"]):0.2f}, {row["Narrative"]}')
+                            elif row['To'] == account.name:
+                                print(f'Date: {row["Date"]}, From: {row["From"]}, Amount: £{float(row["Amount"]):0.2f}, {row["Narrative"]}')
+                    break
+            else:
+                print('No account by that name.')
 
         elif command == 'Exit':
             break
         else:
             print('Invalid command')
+
+main()
