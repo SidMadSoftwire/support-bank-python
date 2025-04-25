4a54246cec0554746033ec11447b9f2bc1358e73
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-25 13:25:17 +0100

    Implement logging

--- support-bank.py
+++ support-bank.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 
 class Account:
     def __init__(self, name, balance):
@@ -7,16 +8,20 @@ class Account:
 
 def update_accounts(file, accounts):
     with open(file, newline='') as csvfile:
+        logging.debug('Updating accounts from %s', file)
         reader = csv.DictReader(csvfile)
         for row in reader:
+            logging.debug('New transaction at %s from %s to %s', row['Date'], row['From'], row['To'])
             payer, payee = False, False
             for account in accounts:
                 payer = False
                 payee = False
-                if account.name == row['From']:
+                if account.name == row['From'] and payer == False:
+                    logging.debug('Taking %s from balance', row['Amount'])
                     account.balance -= float(row['Amount'])
                     payer = True
-                elif account.name == row['To']:
+                elif account.name == row['To'] and payee == False:
+                    logging.debug('Adding %s to balance', row['Amount'])
                     account.balance += float(row['Amount'])
                     payee = True
 
@@ -24,13 +29,18 @@ def update_accounts(file, accounts):
                     break
             else:
                 if not payer:
+                    logging.debug('Creating account with balance -%s', row['Amount'])
                     accounts.append(Account(row['From'], -(float(row['Amount']))))
                 if not payee:
+                    logging.debug('Creating account with balance %s', row['Amount'])
                     accounts.append(Account(row['To'], float(row['Amount'])))
 
 def main():
+    logging.basicConfig(filename='SupportBank.log', filemode='w', level=logging.DEBUG)
+
     accounts = []
     transactions = 'Transactions2014.csv'
+    #transactions = 'DodgyTransactions2015.csv'
     update_accounts(transactions, accounts)
 
     command = ""
@@ -39,25 +49,30 @@ def main():
 
         # List all account balances
         if command == 'List All':
+            logging.debug('Listing all accounts')
             for account in accounts:
                 print(f'Name: {account.name}, Balance: £{account.balance:0.2f}')
 
         # List transactions that involve a specific account
         elif command.startswith('List ') and len(command) >= 6:
+            logging.debug('Listing transactions from %s', command[5:])
             for account in accounts:
                 if command[5:] == account.name:
                     with open(transactions, newline='') as csvfile:
                         reader = csv.DictReader(csvfile)
                         for row in reader:
                             if row['From'] == account.name:
+                                logging.debug('Amount is %s', row['Amount'] )
                                 print(f'Date: {row["Date"]}, To: {row["To"]}, Amount: £-{float(row["Amount"]):0.2f}, {row["Narrative"]}')
                             elif row['To'] == account.name:
+                                logging.debug('Amount is %s', row['Amount'])
                                 print(f'Date: {row["Date"]}, From: {row["From"]}, Amount: £{float(row["Amount"]):0.2f}, {row["Narrative"]}')
                     break
             else:
                 print('No account by that name.')
 
         elif command == 'Exit':
+            logging.debug('Exiting...')
             break
         else:
             print('Invalid command')
