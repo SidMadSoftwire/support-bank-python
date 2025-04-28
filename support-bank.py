1f26baf8a8066cbfe7a1a28d79eedf4a01f0e70b
Siddharth Madhusudan <siddharth.madhusudan@softwire.com>
2025-04-28 09:38:46 +0100

    Add clearer comments

--- support-bank.py
+++ support-bank.py
@@ -12,13 +12,15 @@ def main():
 
     #Read file to dictionary while checking validity
     transactions = []
-    file = 'Transactions2014.csv'
-    #transactions = 'DodgyTransactions2015.csv'
-    open_file(file, transactions)
+    errors = []
+    #file = 'Transactions2014.csv'
+    file = 'DodgyTransactions2015.csv'
+    if not open_file(file, transactions, errors):
+        transactions, errors = [], []
 
     #Calculate account balance
     accounts = []
-    update_accounts(transactions, accounts)
+    update_accounts(transactions, errors, accounts)
 
     command = ""
     while command != "exit":
@@ -30,60 +32,75 @@ def main():
 
         # List transactions that involve a specific account
         elif command.startswith('list ') and len(command) >= 6:
-            list_transactions(transactions, accounts, command[5:])
+            list_transactions(transactions, errors, accounts, command[5:])
+
+        # List transactions that contains errors
+        elif command == 'errors':
+            list_errors(transactions, errors)
 
         elif command == 'exit':
             logging.info('Exiting...')
             break
-        else:
-            print('Invalid command. Try "List All" or "List [name]". Use "Exit" to exit.')
 
-def update_accounts(transactions, accounts):
+        else:
+            print('Invalid command. Try [List All], [List NAME] or [Errors].  Use [Exit] to exit.')
+
+# Update account balance based on transactions, skipping all in errors
+def update_accounts(transactions, errors, accounts):
     logging.info('Updating accounts from current file')
-    for row in transactions:
-        logging.info('New transaction at %s from %s to %s', row['Date'], row['From'], row['To'])
-        payer, payee = False, False
-        for account in accounts:
-            if account.name == row['From'] and payer == False:
-                logging.info('Taking %s from balance', row['Amount'])
-                account.balance -= float(row['Amount'])
-                payer = True
-            elif account.name == row['To'] and payee == False:
-                logging.info('Adding %s to balance', row['Amount'])
-                account.balance += float(row['Amount'])
-                payee = True
+    for idx, row in enumerate(transactions):
+        if idx not in errors:
+            logging.info('New transaction at %s from %s to %s', row['Date'], row['From'], row['To'])
+            payer, payee = False, False
+            for account in accounts:
+                if account.name == row['From'] and payer == False:
+                    logging.info('Taking %s from balance', row['Amount'])
+                    account.balance -= float(row['Amount'])
+                    payer = True
+                elif account.name == row['To'] and payee == False:
+                    logging.info('Adding %s to balance', row['Amount'])
+                    account.balance += float(row['Amount'])
+                    payee = True
 
-            if payer and payee:
-                break
-        else:
-            if not payer:
-                logging.info('Creating account with balance -%s', row['Amount'])
-                accounts.append(Account(row['From'], -(float(row['Amount']))))
-            if not payee:
-                logging.info('Creating account with balance %s', row['Amount'])
-                accounts.append(Account(row['To'], float(row['Amount'])))
+                if payer and payee:
+                    break
+            else:
+                if not payer:
+                    logging.info('Creating account with balance -%s', row['Amount'])
+                    accounts.append(Account(row['From'], -(float(row['Amount']))))
+                if not payee:
+                    logging.info('Creating account with balance %s', row['Amount'])
+                    accounts.append(Account(row['To'], float(row['Amount'])))
 
+# List all account balances
 def list_all(accounts):
     logging.info('Listing all accounts')
-    for account in accounts:
-        print(f'Name: {account.name}, Balance: £{account.balance:0.2f}')
+    if len(accounts) == 0:
+        print("No accounts found")
+    else:
+        for account in accounts:
+            print(f'Name: {account.name}, Balance: £{account.balance:0.2f}')
 
-def list_transactions(transactions, accounts, name):
+# List all transactions by a given account (if it exists), skipping any in errors
+def list_transactions(transactions, errors, accounts, name):
     logging.info('Listing transactions from %s', name)
     for account in accounts:
         if name == account.name.lower():
-            for row in transactions:
-                if row['From'] == account.name:
-                    print(
-                        f'Date: {row["Date"]}, To: {row["To"]}, Amount: £-{float(row["Amount"]):0.2f}, {row["Narrative"]}')
-                elif row['To'] == account.name:
-                    print(
-                        f'Date: {row["Date"]}, From: {row["From"]}, Amount: £{float(row["Amount"]):0.2f}, {row["Narrative"]}')
+            for idx, row in enumerate(transactions):
+                if idx not in errors:
+                    if row['From'] == account.name:
+                        print(
+                            f'Date: {row["Date"]}, To: {row["To"]}, Amount: £-{float(row["Amount"]):0.2f}, {row["Narrative"]}')
+                    elif row['To'] == account.name:
+                        print(
+                            f'Date: {row["Date"]}, From: {row["From"]}, Amount: £{float(row["Amount"]):0.2f}, {row["Narrative"]}')
             break
     else:
-        print('No account by that name. Check accounts with "List ALl".')
+        print('No account by that name. Check accounts with "List All".')
 
-def open_file(file, transactions):
+# Open given file and write it to transactions.
+# If error, user can choose to skip (note line in errors) or cancel the whole file, which returns False
+def open_file(file, transactions, errors):
     with open(file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -91,12 +108,30 @@ def open_file(file, transactions):
                 datetime.strptime(row['Date'], '%d/%m/%Y')
                 float(row['Amount'])
             except ValueError:
-                print("Oh no!")
-                break
-                # TODO
+                print("The following transaction include data in the wrong format :")
+                print(row)
+                print("Would you like to [skip] this line and continue reading the file?")
+                print("Or would you rather [cancel] reading the file entirely?")
+                command = ''
+                while command not in ['skip', 'cancel']:
+                    command = input('What do?: ').lower()
+                    if command == 'skip':
+                        errors.append(len(transactions))
+                        transactions.append(row)
+                    elif command == 'cancel':
+                        return False
+                    else:
+                        print('Invalid command. Would you like to [skip] or [cancel]?')
             else:
                 transactions.append(row)
+    return True
 
+# List all transactions with errors
+def list_errors(transactions, errors):
+    for idx, row in enumerate(transactions):
+        if idx in errors:
+            print(
+                f'Date: {row["Date"]}, From: {row["From"]}, Amount: £{row["Amount"]}, {row["Narrative"]}')
 
 
 main()
